ec34b7e86ca749483d4e21a0eaf6a1867b098651
Pedro Gabriel Vilar <pedro.santos589@etec.sp.gov.br>
2019-09-18 08:22:58 -0300

    blá

---
+++ app/src/main/java/br/com/etecia/appbandio/JanelaDoisActivity.java
@@ -0,0 +1,13 @@
+package br.com.etecia.appbandio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JanelaDoisActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.janela_dois_layout);
+    }
+}
